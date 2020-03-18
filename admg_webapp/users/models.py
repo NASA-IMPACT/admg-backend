dc19ae9b958a3ebd4a808fa0c55d916b264d32bf
Prasanna Koirala <pkoiralap@gmail.com>
2020-03-18 16:36:10 -0500

    added roles in the user model

--- admg_webapp/users/models.py
+++ admg_webapp/users/models.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, IntegerField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
+AVAILABLE_ROLES = ((1, 'Admin'), (2, 'Staff'))
+
 
 class User(AbstractUser):
+    """
+    user model for admg users
+    """
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = CharField(_("Name of User"), max_length=255)
+    role = IntegerField(choices=AVAILABLE_ROLES, default=2)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
