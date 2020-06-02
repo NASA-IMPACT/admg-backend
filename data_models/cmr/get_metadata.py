de8f5b83884d73cf2a1a70ffc048108fbbb65624
Carson Davis <CarsonReidDavis@gmail.com>
2020-06-02 09:34:37 -0500

    add flight extractor, refactor imports and get_campaign

--- data_models/cmr/get_metadata.py
+++ data_models/cmr/get_metadata.py
@@ -1,4 +1,12 @@
-from helper_functions import * 
+from helper_functions import (
+    campaign_xlm_to_json,
+    date_filter,
+    extract_collection_periods,
+    extract_daacs,
+    extract_region_description,
+    general_extractor,
+    ingest_campaign,
+) 
 from datetime import datetime
 import pickle
 
@@ -23,22 +31,24 @@ def query_api(short_name):
     return campaign_metadata
 
 
-def get_campaign(short_name):
+def get_campaign(campaign_short_name):
     """Extracts partial campaign metadata from the cmr download. Note that the
     general_extractor can be used to extract additional metadata not currently
     used.
 
     Args:
-        short_name (str): Project/Campaign short_name
+        campaign_short_name (str): Project/Campaign short_name
 
     Returns:
         dict: dictionary containing campaign and top-level table metadata
     """
 
     # TODO: replace this with the final location in the db we decide on
-    campaign_metadata = pickle.load(open(f'cmr_data-{short_name}', 'rb'))
+    campaign_metadata = pickle.load(open(f'cmr_data-{campaign_short_name}', 'rb'))
 
     db = {'campaign':{}}
+
+    db['campaign']['short_name'] = campaign_short_name
 
     db['campaign']['gcmd_region'] = extract_region_description(campaign_metadata)
 
@@ -53,21 +63,33 @@ def get_campaign(short_name):
     return db
 
 
-def get_deployment(campaign_metadata, dep_start, dep_end):
+def get_deployment_and_cp(campaign_short_name, dep_start, dep_end):
+    """Filters the CMR campaign_metadata on a date range and then creates a deployment
+    entry as well as a linked list of collection_periods with associated platform and
+    instrument metadata.
+
+    Args:
+        campaign_metadata ([type]): [description]
+        campaign_short_name (str): Project/Campaign short_name
+        dep_start (datetime): deployment start date
+        dep_end (datetime): deployment end date
+
+    Returns:
+        dict: dictionary containing deployment and collection period data
+    """
+
+    # TODO: replace this with the final location in the db we decide on
+    campaign_metadata = pickle.load(open(f'cmr_data-{campaign_short_name}', 'rb'))
+
+    db = {'deployment': {}}
+    db['deployment']['short_name'] = '_&_'.join([campaign_short_name, str(dep_start), str(dep_end)])
+    db['deployment']['foreign-campaign-short_name'] = campaign_short_name
+    db['deployment']['start_date'] = dep_start
+    db['deployment']['end_date'] = dep_end
 
     deployment_metadata = date_filter(campaign_metadata, dep_start, dep_end)
 
+    db['collection_period'] = extract_collection_periods(deployment_metadata)
+    db['collection_period']['foreign-deployment-short_name'] = db['deployment']['short_name']
 
-# user input
-short_name='above'
-dep_start = datetime(2015,1,2)
-dep_end = datetime(2016,1,1)
-
-# query api
-# campaign_trees = ingest_campaign(short_name)
-# campaign_metadata = campaign_xlm_json(campaign_trees)
-campaign_metadata = pickle.load(open('cmr_data','rb'))
-
-# TODO: replace this with save to database
-pickle.dump(campaign_metadata, open('cmr_data','wb'))
-
+    return db
