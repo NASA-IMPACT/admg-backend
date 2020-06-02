91dc457960df5e530e838fbe5b9816906724394d
Carson Davis <CarsonReidDavis@gmail.com>
2020-06-02 09:02:25 -0500

    update get_campaign with gcmd_phenomena and other_resources

--- data_models/cmr/get_metadata.py
+++ data_models/cmr/get_metadata.py
@@ -18,15 +18,44 @@ def query_api(short_name):
     campaign_metadata = campaign_xlm_to_json(campaign_trees)
 
     # TODO: replace this with the final location in the db we decide on
-    pickle.dump(campaign_metadata, open(f'cmr_data-{short_name}','wb'))    
+    pickle.dump(campaign_metadata, open(f'cmr_data-{short_name}', 'wb'))    
 
     return campaign_metadata
 
+
 def get_campaign(short_name):
+    """Extracts partial campaign metadata from the cmr download. Note that the
+    general_extractor can be used to extract additional metadata not currently
+    used.
+
+    Args:
+        short_name (str): Project/Campaign short_name
+
+    Returns:
+        dict: dictionary containing campaign and top-level table metadata
+    """
+
+    # TODO: replace this with the final location in the db we decide on
+    campaign_metadata = pickle.load(open(f'cmr_data-{short_name}', 'rb'))
+
+    db = {'campaign':{}}
+
+    db['campaign']['gcmd_region'] = extract_region_description(campaign_metadata)
+
+    db['campaign']['repositories'] = extract_daacs(campaign_metadata)
+
+    db['campaign']['spatial_bounds'] = general_extractor(campaign_metadata, 'SpatialExtent')
+
+    db['campaign']['gcmd_phenomena'] = general_extractor(campaign_metadata, 'ScienceKeywords')
+
+    db['campaign']['other_resources'] = general_extractor(campaign_metadata, 'RelatedUrls')    
+
+    return db
 
 
 def get_deployment(campaign_metadata, dep_start, dep_end):
 
+    deployment_metadata = date_filter(campaign_metadata, dep_start, dep_end)
 
 
 # user input
