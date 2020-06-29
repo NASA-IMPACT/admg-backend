2831875972686ac36a7cf4084568af3eee5639f9
Carson Davis <CarsonReidDavis@gmail.com>
2020-06-29 16:07:39 -0500

    change FileField to ImageField

--- data_models/models.py
+++ data_models/models.py
@@ -26,10 +26,10 @@ def get_file_path(instance, path):
     return f'{instance.uuid}{os.path.splitext(path)[1]}'
 
 class Image(BaseModel):
-    photo = models.FileField(upload_to=get_file_path, null=True, blank=True)
+    photo = models.ImageField(upload_to=get_file_path, null=True, blank=True)
     short_name = models.CharField(max_length=512, default='', blank=True)
     owner = models.CharField(max_length=512, default='', blank=True)
-    
+
     def __str__(self):
         return self.short_name or self.photo.name
 
