466963425e33393dfdf8978afb25fa8659ee5db8
Carson Davis <CarsonReidDavis@gmail.com>
2020-08-10 17:50:01 -0500

    linting

--- api_app/views/image_view.py
+++ api_app/views/image_view.py
@@ -2,14 +2,12 @@ from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 from rest_framework.parsers import MultiPartParser
 
 from api_app.serializers import ImageSerializer
+from data_models.models import Image
 
 from ..models import DELETE
 from .view_utils import handle_exception, requires_admin_approval
 
-from data_models.models import Image
 
-
-# class ImageUploadView(ListCreateAPIView):
 class ImageListCreateView(ListCreateAPIView):
     parser_class = (MultiPartParser,)
     queryset = Image.objects.all()
