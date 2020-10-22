cf5e1a7f93f672983af20d169eb37c8b663b06c8
Carson Davis <CarsonReidDavis@gmail.com>
2020-10-22 10:34:34 -0500

    add validation endpoint for change requests

--- api_app/views/change_view.py
+++ api_app/views/change_view.py
@@ -136,6 +136,16 @@ def ChangeApproveRejectView(approve_or_reject):
 
     return View.as_view()
 
+class ChangeValidationView(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = [STAFF]
+    
+    @handle_exception
+    def post(self, request, *args, **kwargs):
+        uuid = kwargs.get("uuid")
+        instance = _validate_update(request, uuid)
+        return instance.validate()
+
 
 class ChangePushView(APIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
