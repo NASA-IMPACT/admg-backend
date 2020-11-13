d05c83fd063c22ecb562eea8997fbaa090ef7765
Carson Davis <CarsonReidDavis@gmail.com>
2020-11-13 00:07:14 -0600

    fix strip() function

--- data_models/utils/clean.py
+++ data_models/utils/clean.py
@@ -15,7 +15,7 @@ def strip_all_columns(db):
         table = db[table_name]
         text_col_df = table.select_dtypes(['object'])
         columns = [col for col in text_col_df.columns if 'date' not in col]
-        table[columns] = table[columns].apply(lambda x: x.strip() if isinstance(x, str) else x)
+        table[columns] = table[columns].applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
         db[table_name] = table
 
