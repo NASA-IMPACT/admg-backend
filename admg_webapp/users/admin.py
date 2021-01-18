95c2611db968202700078a647d6f8a69f4c7c30b
Anthony Lukach <anthonylukach@gmail.com>
2021-01-18 14:04:24 -0700

    Rm unused user form

--- admg_webapp/users/admin.py
+++ admg_webapp/users/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
-from admg_webapp.users.forms import UserChangeForm, UserCreationForm
-
 User = get_user_model()
 
 
