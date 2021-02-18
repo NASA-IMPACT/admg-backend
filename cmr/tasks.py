0e26ac81db0ffa629bccb48db1d8a77d572975ca
Carson Davis <CarsonReidDavis@gmail.com>
2021-02-18 11:31:38 -0600

    add default task for testing celery

--- cmr/tasks.py
+++ cmr/tasks.py
@@ -5,3 +5,7 @@ from cmr.doi_matching import DoiMatcher
 def match_dois(table_name, uuid):
     matcher = DoiMatcher()
     return matcher.generate_recommendations(table_name, uuid)
+
+@shared_task
+def add(a,b):
+    return a+b
