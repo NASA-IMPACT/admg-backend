0da271f0fae4f04df4e4f137f67bb8fe6a25a5da
Carson Davis <carsonreiddavis@gmail.com>
2021-03-04 00:20:14 -0600

    remove comprehension from alias serializer

--- data_models/serializers.py
+++ data_models/serializers.py
@@ -62,7 +62,7 @@ class GetAliasSerializer(BaseSerializer):
     aliases = serializers.SerializerMethodField(read_only=True)
 
     def get_aliases(self, obj):
-        return [alias.uuid for alias in obj.aliases.all()]
+        return get_uuids(obj.aliases)
 
 class GetDoiSerializer(BaseSerializer):
     dois = serializers.SerializerMethodField(read_only=True)    
@@ -317,7 +317,7 @@ class PlatformSerializer(GetAliasSerializer, GetDoiSerializer):
     campaigns = serializers.ListField(read_only=True)
 
     def get_collection_periods(self, obj):
-        return get_uuids(obj.collection_periods) 
+        return get_uuids(obj.collection_periods)
 
     class Meta:
         model = models.Platform
