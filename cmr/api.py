a1ca839ff64f27b55658ea21c05bc950e0a1429a
Carson Davis <carsonreiddavis@gmail.com>
2021-03-09 13:19:38 -0600

    add updated api

--- cmr/api.py
+++ cmr/api.py
@@ -1,27 +1,31 @@
 import json
+import os
+import sys
+
 import requests
 
 from cmr.config import credentials as CREDENTIALS
+
+DEFAULT_APPROVAL_NOTE = 'this action was perfomed using the API'
 
 
 class Api:
     def __init__(self):
         """Initializes the api by setting up the credentials and
         getting the token from one of the various servers
-
-        Args:
-            deployment (str): 'test', 'production', or 'local'
         """
+
 
         self.credentials = CREDENTIALS
         self.base_url = f"{self.credentials['server']}/api/"
         self._make_connection()
 
+
     def _make_connection(self):
         """Connects to the database and sets the headers for future api requests.
 
         Raises:
-            ValueError: Will raise error if connection fails. 
+            ValueError: Will raise error if connection fails.
         """
 
         token_url = f"{self.credentials['server']}/authenticate/token/"
@@ -30,51 +34,80 @@ class Api:
             data=self.credentials['data'],
             auth=(self.credentials['client_id'], self.credentials['client_secret']),
         )
-        
-        response = json.loads(response.text)
-        if response.get('error') == 'invalid_client':
-            raise ValueError("Invalid Client. Check token details in config file.")
-        
-        access_token = response['access_token']
 
+        response_dict = self._generate_response_dict(response)
+        access_token = response_dict['access_token']
 
         self.headers = {
             'Authorization': f'Bearer {access_token}',
             'Content-Type': 'application/json',
         }
 
-    def _approve_change_object(self, response):
+
+    @staticmethod
+    def _generate_response_dict(response):
+        """Takes a response and raises any HTTPErrors. If no errors are found, it will
+        convert the response text to a dict.
+
+        Args:
+            response (response): response
+
+        Returns:
+            response_dict [dict]: dictionary of the response.text
+        """
+
+        response.raise_for_status()
+        response_dict =  response.json()
+
+        return response_dict
+
+
+    def _approve_change_object(self, response_dict, notes=DEFAULT_APPROVAL_NOTE):
         """Change objects must pass through an approval process in order to be added
-        to the database. This function streamlines everystep of the approval process
-        and results in the draft change object being added to the database.
+        to the database. This function streamlines every step of the approval process
+        and results in the draft change object being published to the database.
 
         Args:
             response (response): Response object from a python request. This should
-               come from the update, delete, or create methods.
+                come from the update, delete, or create methods.
+            notes (str): Notes to be left in the ApprovalLog. Defaults to this action
+                was perfomed using the API
 
         Returns:
             response : Response object from the approval process.
         """
 
-        # get the change_request_uuid
-        response = json.loads(response.text)
-        uuid = response['data']['uuid']
+        uuid = response_dict['data']['uuid']
+        notes_dict = {'notes': notes}
 
-        push_response = json.loads(requests.post(
-            f'{self.base_url}change_request/{uuid}/push', headers=self.headers
-        ).text)
+        for approval_step in ['submit', 'claim', 'review', 'claim', 'publish']:
+            response_dict = self.post(f'change_request/{uuid}/{approval_step}', notes_dict)
 
-        if push_response.get('success'):
-            approve_response = json.loads(
-                requests.post(
-                    f'{self.base_url}change_request/{uuid}/approve', headers=self.headers
-                ).text
-            )
-            final_response = approve_response
-        else:
-            final_response = push_response
+            if not response_dict['success']:
+                raise Exception(response_dict)
 
-        return final_response
+        return response_dict
+
+
+    def post(self, endpoint, data):
+        """Generic POST function. Not really intended to be used directly in any routine ingest pipelines,
+        as self.create and self.update fulfil that role. However this is useful for testing out
+        new views during development, and is used during the approval process.
+
+        Args:
+            endpoint (str): API endpoing such as 'partner_org'
+            data ([dict]): dictionary of data matching endpoint requirements
+
+        Returns:
+            dict: API response dictionary
+        """
+
+        post_url = f'{self.base_url}{endpoint}'
+        response = requests.post(post_url, data=json.dumps(data), headers=self.headers)
+        response_dict = self._generate_response_dict(response)
+
+        return response_dict
+
 
     def get(self, endpoint):
         """Takes an ADMG endpoint as a string and runs a get request
@@ -88,8 +121,10 @@ class Api:
 
         get_url = f'{self.base_url}{endpoint}'
         response = requests.get(get_url, headers=self.headers)
+        response_dict = self._generate_response_dict(response)
 
-        return json.loads(response.text)
+        return response_dict
+
 
     def delete(self, endpoint):
         """Takes an endpoint and deletes the object in the db
@@ -100,8 +135,10 @@ class Api:
 
         delete_url = f'{self.base_url}{endpoint}'
         response = requests.delete(delete_url, headers=self.headers)
+        response_dict = self._generate_response_dict(response)
 
-        self._approve_change_object(response)
+        self._approve_change_object(response_dict)
+
 
     def update(self, endpoint, data, draft=False):
         """Takes and endpoint and some data and updates the object in the db
@@ -113,11 +150,12 @@ class Api:
 
         post_url = f'{self.base_url}{endpoint}'
         response = requests.patch(post_url, data=json.dumps(data), headers=self.headers)
+        response_dict = self._generate_response_dict(response)
 
         if draft:
-            return json.loads(response.text)
+            return response_dict
         else:
-            return self._approve_change_object(response)
+            return self._approve_change_object(response_dict)
 
 
     def create(self, endpoint, data, draft=False):
@@ -134,12 +172,12 @@ class Api:
 
         post_url = f'{self.base_url}{endpoint}'
         response = requests.post(post_url, data=json.dumps(data), headers=self.headers)
+        response_dict = self._generate_response_dict(response)
 
-        response_dict = json.loads(response.text)
         if not(response_dict['success']) and 'this short name already exists' in response_dict['message']:
             return f'the following entry already existed {endpoint=} {data=}'
 
         if draft:
             return response_dict
         else:
-            return self._approve_change_object(response)
+            return self._approve_change_object(response_dict)
