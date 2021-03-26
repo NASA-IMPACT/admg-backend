9a422e377ca95a6a3ee8bf2e9762a3bbf4ec3447
Rachel Wegener <rachel.e.wegener@gmail.com>
2021-03-26 07:51:31 -0600

    remove comments

--- admin_ui/filters.py
+++ admin_ui/filters.py
@@ -7,12 +7,6 @@ class ChangeStatusFilter(django_filters.FilterSet):
     short_name = django_filters.CharFilter(
         label="Short Name", field_name="update__short_name", lookup_expr="iexact"
     )
-    # nasa_led = django_filters.BooleanFilter(
-    #     label="NASA Led", field_name="update__nasa_led"
-    # )
-    # funding_agency = django_filters.Filter(
-    #     label="Funding Agency", field_name="update__funding_agency"
-    # )
 
     class Meta:
         model = Change
