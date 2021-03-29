397b625411bf02eaa6c9a7d9c40509d58d8f61d7
Rachel Wegener <35503632+rwegener2@users.noreply.github.com>
2021-03-29 09:50:52 -0600

    update short name match to icontains
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>

--- admin_ui/filters.py
+++ admin_ui/filters.py
@@ -5,7 +5,7 @@ from api_app.models import Change
 
 class ChangeStatusFilter(django_filters.FilterSet):
     short_name = django_filters.CharFilter(
-        label="Short Name", field_name="update__short_name", lookup_expr="iexact"
+        label="Short Name", field_name="update__short_name", lookup_expr="icontains"
     )
 
     class Meta:
