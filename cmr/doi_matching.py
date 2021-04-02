53a788f306836810872caecaab896eea69678c30
Carson Davis <carsonreiddavis@gmail.com>
2021-04-01 20:22:35 -0500

    change status int to var

--- cmr/doi_matching.py
+++ cmr/doi_matching.py
@@ -314,7 +314,7 @@ class DoiMatcher():
                 content_type=ContentType.objects.get(model='doi'),
                 model_instance_uuid=None,
                 update=json.loads(json.dumps(doi)),
-                status=0,
+                status=CREATED_CODE,
                 action=CREATE
             )
             doi_obj.save()
