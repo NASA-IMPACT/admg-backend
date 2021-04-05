1208bc08479679786182168da1fcab0c12aa3745
Marc Farra <marcfarra@gmail.com>
2021-04-05 19:32:21 +0300

    Send geojson and raw value to widget

--- admin_ui/widgets.py
+++ admin_ui/widgets.py
@@ -1,11 +1,15 @@
 import json
 import logging
 
+from django.conf import settings
+from django.contrib.gis import gdal
 from django.contrib.gis.forms import widgets
 from django.forms.widgets import Textarea
 from data_models.serializers import get_geojson_from_bb
 from django.contrib.gis.gdal import SpatialReference, CoordTransform
+from django.contrib.gis.gdal.error import GDALException
 from django.contrib.gis.geos import GEOSException, GEOSGeometry
+from django.utils import translation
 
 logger = logging.getLogger(__name__)
 
@@ -17,41 +21,29 @@ def get_json_representation(value: str):
     except json.JSONDecodeError:
         return get_geojson_from_bb(value)
 
-
-# def get_custom_bbox_representation(value: str):
-
-
 class BoundingBoxWidget(widgets.OpenLayersWidget):
+    template_name = "widgets/custommap.html"
+
     map_srid = 3857
 
-    # NOTES: I still have no idea when format_value(), serialize(), and deserialize() are called.
-    # It seems like they all get called on every page load...
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
 
-    # def format_value(self, value):
-    #     # return "45, 44, -121, -124"
-    #     if isinstance(value, str):
-    #         try:
-    #             value = GEOSGeometry(value)
-    #         except (GEOSException, ValueError):
-    #             return value
+        geom_type = gdal.OGRGeomType(self.attrs['geom_type']).name
+        context.update(self.build_attrs(self.attrs, {
+            'name': name,
+            'module': 'geodjango_%s' % name.replace('-', '_'),  # JS-safe
+            'serialized': self.to_geojson(value),
+            'extent': value,
+            'geom_type': 'Geometry' if geom_type == 'Unknown' else geom_type,
+            'STATIC_URL': settings.STATIC_URL,
+            'LANGUAGE_BIDI': translation.get_language_bidi(),
+            **(attrs or {}),
+        }))
+        return context
 
-    #     value.transform(
-    #         CoordTransform(SpatialReference(self.map_srid), SpatialReference(4326))
-    #     )
 
-    #     return ",".join(
-    #         [
-    #             max(str(lat) for lat, lng in value.tuple[0]),
-    #             min(str(lat) for lat, lng in value.tuple[0]),
-    #             max(str(lng) for lat, lng in value.tuple[0]),
-    #             min(str(lng) for lat, lng in value.tuple[0]),
-    #         ]
-    #     )
-
-    # def serialize(self, value):
-    #     return super().serialize(value)
-
-    def deserialize(self, value):
+    def to_geojson(self, value):
         """ Create a geometry object from string """
         try:
             geom = GEOSGeometry(get_json_representation(value))
@@ -61,7 +53,7 @@ class BoundingBoxWidget(widgets.OpenLayersWidget):
                         SpatialReference(geom.srid), SpatialReference(self.map_srid)
                     )
                 )
-            return geom
-        except (GEOSException, ValueError, TypeError) as err:
+            return geom.json
+        except (GEOSException, GDALException, ValueError, TypeError) as err:
             logger.error("Error creating geometry from value '%s' (%s)", value, err)
         return None
