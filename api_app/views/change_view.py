fffa297e80aac730bfa5bb30e8921256864e4fd0
Anthony Lukach <anthonylukach@gmail.com>
2021-04-06 09:31:50 -0600

    Undo changes to api_app

--- api_app/views/change_view.py
+++ api_app/views/change_view.py
@@ -1,7 +1,10 @@
 from drf_yasg import openapi
 
 from rest_framework import permissions
-from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveUpdateAPIView
+)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -20,7 +23,6 @@ def _validate_update(request, uuid):
         raise Exception("Change has already been published. Make a new change request")
     return instance
 
-
 class ApprovalLogListView(ListAPIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
     required_scopes = [STAFF]
@@ -30,11 +32,13 @@ class ApprovalLogListView(ListAPIView):
 
     @handle_exception
     def get(self, request, *args, **kwargs):
-        self.queryset = ApprovalLog.objects.filter(**request.query_params.dict())
+        self.queryset = ApprovalLog.objects.filter(
+            **request.query_params.dict()
+        )
         return super().get(request, *args, **kwargs)
 
 
-class CampaignListView(ListAPIView):
+class ChangeListView(ListAPIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
     required_scopes = [STAFF]
 
@@ -43,7 +47,9 @@ class CampaignListView(ListAPIView):
 
     @handle_exception
     def get(self, request, *args, **kwargs):
-        self.queryset = Change.objects.filter(**request.query_params.dict())
+        self.queryset = Change.objects.filter(
+            **request.query_params.dict()
+        )
         return super().get(request, *args, **kwargs)
 
 
@@ -73,7 +79,9 @@ class ChangeListUpdateView(RetrieveUpdateAPIView):
 notes_param = openapi.Schema(
     type=openapi.TYPE_OBJECT,
     required=[],
-    properties={"notes": openapi.Schema(type=openapi.TYPE_STRING)},
+    properties={
+        "notes": openapi.Schema(type=openapi.TYPE_STRING)
+    },
 )
 
 
@@ -83,10 +91,9 @@ class ChangeValidationView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        uuid = kwargs.get("uuid")
+        uuid = kwargs.get('uuid')
         instance = _validate_update(request, uuid)
         return instance.validate()
-
 
 class ChangeSubmitView(APIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
@@ -94,12 +101,12 @@ class ChangeSubmitView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        instance = Change.objects.get(uuid=kwargs.get("uuid"))
+        instance = Change.objects.get(uuid=kwargs.get('uuid'))
         response = instance.submit(
-            user=request.auth.user, notes=request.data.get("notes", "")
+            user = request.auth.user,
+            notes= request.data.get('notes', '')
         )
         return Response(response)
-
 
 class ChangeClaimView(APIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
@@ -107,12 +114,12 @@ class ChangeClaimView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        instance = Change.objects.get(uuid=kwargs.get("uuid"))
+        instance = Change.objects.get(uuid=kwargs.get('uuid'))
         response = instance.claim(
-            user=request.auth.user, notes=request.data.get("notes", "")
+            user = request.auth.user,
+            notes= request.data.get('notes', '')
         )
         return Response(response)
-
 
 class ChangeUnclaimView(APIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
@@ -120,12 +127,12 @@ class ChangeUnclaimView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        instance = Change.objects.get(uuid=kwargs.get("uuid"))
+        instance = Change.objects.get(uuid=kwargs.get('uuid'))
         response = instance.unclaim(
-            user=request.auth.user, notes=request.data.get("notes", "")
+            user = request.auth.user,
+            notes= request.data.get('notes', '')
         )
         return Response(response)
-
 
 class ChangeReviewView(APIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
@@ -133,9 +140,10 @@ class ChangeReviewView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        instance = Change.objects.get(uuid=kwargs.get("uuid"))
+        instance = Change.objects.get(uuid=kwargs.get('uuid'))
         response = instance.review(
-            user=request.auth.user, notes=request.data.get("notes", "")
+            user = request.auth.user,
+            notes= request.data.get('notes', '')
         )
         return Response(response)
 
@@ -146,9 +154,10 @@ class ChangeRejectView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        instance = Change.objects.get(uuid=kwargs.get("uuid"))
+        instance = Change.objects.get(uuid=kwargs.get('uuid'))
         response = instance.reject(
-            user=request.auth.user, notes=request.data.get("notes", "")
+            user = request.auth.user,
+            notes= request.data.get('notes', '')
         )
         return Response(response)
 
@@ -159,8 +168,9 @@ class ChangePublishView(APIView):
 
     @handle_exception
     def post(self, request, *args, **kwargs):
-        instance = Change.objects.get(uuid=kwargs.get("uuid"))
+        instance = Change.objects.get(uuid=kwargs.get('uuid'))
         response = instance.publish(
-            user=request.auth.user, notes=request.data.get("notes", "")
+            user = request.auth.user,
+            notes= request.data.get('notes', '')
         )
         return Response(response)
