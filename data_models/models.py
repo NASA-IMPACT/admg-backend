7e0e6d7cb11f3b4ebea2f3617f8960b8ac5b282c
Carson Davis <carsonreiddavis@gmail.com>
2021-05-20 09:54:40 -0500

    add custom str for gcmdplatform

--- data_models/models.py
+++ data_models/models.py
@@ -228,8 +228,7 @@ class GcmdProject(BaseModel):
     gcmd_uuid = models.UUIDField(unique=True)
 
     def __str__(self):
-        categories =  [self.bucket, self.long_name, self.short_name]
-        return create_gcmd_str(categories)
+        return self.short_name or self.long_name or self.bucket
 
     class Meta():
         ordering = ('short_name', )
@@ -262,7 +261,8 @@ class GcmdPlatform(BaseModel):
     gcmd_uuid = models.UUIDField(unique=True)
 
     def __str__(self):
-        return self.short_name or self.long_name or self.category
+        categories =  [self.bucket, self.long_name, self.short_name]
+        return create_gcmd_str(categories)
 
     class Meta():
         ordering = ('short_name', )
