3a8c939bbf4cfc550a9a7380c2072a89dbc9db39
Anthony Lukach <anthonylukach@gmail.com>
2021-05-24 12:04:10 -0600

    Retain model name

--- admin_ui/forms.py
+++ admin_ui/forms.py
@@ -102,7 +102,7 @@ class DoiForm(forms.Form):
     platforms = ChangeMultipleChoiceField(
         dest_model=data_models.Platform, required=False
     )
-    cdpis = forms.MultipleChoiceField(label="CDPIs", required=False)
+    collection_periods = forms.MultipleChoiceField(label="CDPIs", required=False)
     keep = forms.NullBooleanField(
         help_text="Mark as saved or deleted",
         widget=IconBoolean,
@@ -138,7 +138,7 @@ class DoiFormSet(forms.formset_factory(DoiForm, extra=0)):
             "instruments": ChangeMultipleChoiceField.get_queryset_for_model(
                 data_models.Instrument
             ),
-            "cdpis": ChangeMultipleChoiceField.get_queryset_for_model(
+            "collection_periods": ChangeMultipleChoiceField.get_queryset_for_model(
                 data_models.CollectionPeriod
             ),
         }
