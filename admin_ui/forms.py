8e4dc088b010f475ad450f3720de43e221931fe9
Rachel Wegener <rachel.e.wegener@gmail.com>
2021-05-24 13:24:57 -0500

    change order of DOI table columns

--- admin_ui/forms.py
+++ admin_ui/forms.py
@@ -96,11 +96,11 @@ class DoiForm(forms.Form):
     campaigns = ChangeMultipleChoiceField(
         dest_model=data_models.Campaign, required=False
     )
-    instruments = ChangeMultipleChoiceField(
-        dest_model=data_models.Instrument, required=False
-    )
     platforms = ChangeMultipleChoiceField(
         dest_model=data_models.Platform, required=False
+    )
+    instruments = ChangeMultipleChoiceField(
+        dest_model=data_models.Instrument, required=False
     )
     collection_periods = forms.MultipleChoiceField(label="CDPIs", required=False)
     keep = forms.NullBooleanField(
@@ -148,10 +148,7 @@ class DoiFormSet(forms.formset_factory(DoiForm, extra=0)):
         }
 
     def get_form_kwargs(self, index):
-        return {
-            **super().get_form_kwargs(index),
-            "choices": self.choices,
-        }
+        return {**super().get_form_kwargs(index), "choices": self.choices}
 
 
 class TableInlineFormSetHelper(FormHelper):
