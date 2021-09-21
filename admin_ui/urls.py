4a0dfe79f6a8c74895a4cdf7ee7d75a2cd60216a
Prasanna Koirala <pkoiralap@gmail.com>
2021-09-21 17:00:16 -0500

    create separate urls for published items

--- admin_ui/urls.py
+++ admin_ui/urls.py
@@ -4,13 +4,21 @@ from django.urls import path
 from django.views.generic.base import TemplateView, RedirectView
 
 from . import views
+from . import published_views
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     # Actions
     path("actions/deploy-admin", views.trigger_deploy, name="mi-trigger-deploy"),
     path("", views.SummaryView.as_view(), name="mi-summary"),
-    path("campaigns", views.CampaignListView.as_view(), name="mi-campaign-list"),
+
+    path(
+        "campaigns/published",
+        published_views.CampaignListView.as_view(),
+        name="mi-campaign-list-published"
+    ),
+
+    path("campaigns/drafts", views.CampaignListView.as_view(), name="mi-campaign-list"),
     path(
         "campaigns/<uuid:pk>",
         views.CampaignDetailView.as_view(),
@@ -26,17 +34,41 @@ urlpatterns = [
         views.DoiApprovalView.as_view(),
         name="mi-doi-approval",
     ),
-    path("platforms", views.PlatformListView.as_view(), name="mi-platform-list"),
-    path("instruments", views.InstrumentListView.as_view(), name="mi-instrument-list"),
+    path("platforms/published", published_views.PlatformListView.as_view(), name="mi-platform-list-published"),
+    path("platforms/drafts", views.PlatformListView.as_view(), name="mi-platform-list"),
+
     path(
-        "organizations",
+        "instruments/published",
+        published_views.InstrumentListView.as_view(),
+        name="mi-instrument-list-published"
+    ),
+    path("instruments/drafts", views.InstrumentListView.as_view(), name="mi-instrument-list"),
+
+    path(
+        "organizations/published",
+        published_views.PartnerOrgListView.as_view(),
+        name="mi-organization-list-published",
+    ),
+    path(
+        "organizations/drafts",
         views.PartnerOrgListView.as_view(),
         name="mi-organization-list",
     ),
+
     path(
-        "websites",
+        "websites/published",
+        published_views.WebsiteListView.as_view(),
+        name="website-list-published",
+    ),
+    path(
+        "websites/drafts",
         views.WebsiteListView.as_view(),
         name="website-list",
+    ),
+    path(
+        "aliases/published",
+        published_views.AliasListView.as_view(),
+        name="alias-list-published",
     ),
     path(
         "aliases",
@@ -62,27 +94,52 @@ urlpatterns = [
         name="lf-base",
     ),
     path(
-        "limitedfields/gcmd",
+        "limitedfields/gcmd/published",
+        published_views.LimitedFieldGCMDListView.as_view(),
+        name="lf-gcmd-list-published",
+    ),
+    path(
+        "limitedfields/gcmd/draft",
         views.LimitedFieldGCMDListView.as_view(),
         name="lf-gcmd-list",
     ),
     path(
-        "limitedfields/science",
+        "limitedfields/science/published",
+        published_views.LimitedFieldScienceListView.as_view(),
+        name="lf-science-list-published",
+    ),
+    path(
+        "limitedfields/science/drafts",
         views.LimitedFieldScienceListView.as_view(),
         name="lf-science-list",
     ),
     path(
-        "limitedfields/measurementplatform",
+        "limitedfields/measurementplatform/published",
+        published_views.LimitedFieldMeasurmentPlatformListView.as_view(),
+        name="lf-measure-platform-list-published",
+    ),
+    path(
+        "limitedfields/measurementplatform/drafts",
         views.LimitedFieldMeasurmentPlatformListView.as_view(),
         name="lf-measure-platform-list",
     ),
     path(
-        "limitedfields/regionseason",
+        "limitedfields/regionseason/published",
+        published_views.LimitedFieldRegionSeasonListView.as_view(),
+        name="lf-region-season-list-published",
+    ),
+    path(
+        "limitedfields/regionseason/drafts",
         views.LimitedFieldRegionSeasonListView.as_view(),
         name="lf-region-season-list",
     ),
     path(
-        "limitedfields/website",
+        "limitedfields/website/published",
+        published_views.LimitedFieldWebsiteListView.as_view(),
+        name="lf-website-list-published",
+    ),
+    path(
+        "limitedfields/website/drafts",
         views.LimitedFieldWebsiteListView.as_view(),
         name="lf-website-list",
     ),
