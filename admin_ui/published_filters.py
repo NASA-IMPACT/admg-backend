40eb5deecc09f1bd39f3e556d0de8891f939d828
Prasanna Koirala <pkoiralap@gmail.com>
2021-10-14 18:22:18 -0500

    published filter bug fix

--- admin_ui/published_filters.py
+++ admin_ui/published_filters.py
@@ -3,12 +3,13 @@ import django_filters
 from api_app.models import Change
 from data_models import models
 
+
 def GenericPublishedListFilter(model_name):
+
     class GenericFilterClass(django_filters.FilterSet):
         short_name = django_filters.CharFilter(
-            label="Short Name", field_name="update__short_name", lookup_expr="icontains"
+            label="Short Name", field_name="short_name", lookup_expr="icontains"
         )
-
         class Meta:
             model = getattr(models, model_name)
             fields = ["short_name"]
