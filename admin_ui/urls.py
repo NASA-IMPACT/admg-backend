1c383c055d99c8309b8dec7dd9786a20bba57230
Carson Davis <carsonreiddavis@gmail.com>
2021-10-21 15:37:11 -0500

    remove deprecated comments

--- admin_ui/urls.py
+++ admin_ui/urls.py
@@ -11,14 +11,8 @@ from .published_urls import published_urls
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     # Actions
-    path("actions/deploy-admin",
-        views.trigger_deploy,
-        name="trigger-deploy"
-    ),
-    path("",
-        views.SummaryView.as_view(),
-        name="summary"
-    ),
+    path("actions/deploy-admin", views.trigger_deploy, name="trigger-deploy"),
+    path("", views.SummaryView.as_view(), name="summary"),
     path(
         "campaigns/<uuid:pk>",
         views.CampaignDetailView.as_view(),
@@ -33,12 +27,8 @@ urlpatterns = [
         "campaigns/<uuid:pk>/doi-approval",
         views.DoiApprovalView.as_view(),
         name="doi-approval",
-    ),  
-    path(
-        "drafts/add/<str:model>",
-        views.ChangeCreateView.as_view(),
-        name="change-add"
     ),
+    path("drafts/add/<str:model>", views.ChangeCreateView.as_view(), name="change-add"),
     path(
         "drafts/edit/<uuid:pk>",
         views.ChangeUpdateView.as_view(),
@@ -48,13 +38,12 @@ urlpatterns = [
         "drafts/edit/<uuid:pk>/transition",
         views.ChangeTransition.as_view(),
         name="change-transition",
-    ),   
+    ),
     path(
         "tbd",
         TemplateView.as_view(template_name="api_app/to_be_developed.html"),
         name="to-be-developed",
     ),
-
 ]
 
 from admin_ui.views import generate_base_list_view
@@ -63,18 +52,18 @@ from admin_ui.views import generate_base_list_view
 
 
 auto_url_keys = [
-    "PlatformType", #parent
-    "MeasurementType", #parent
-    "MeasurementStyle", #parent
-    "HomeBase", #location
-    "FocusArea", #url
-    "Season", # none
-    "Repository", # gcmd_uuid
-    "MeasurementRegion", # example, gcmd_uuid
-    "GeographicalRegion", # example, gcmd_uuid
-    "GeophysicalConcept", # example, gcmd_uuid
-    "PartnerOrg", # website
-    "Alias", # NO INHERITEANCE, short_name, how to do model?
+    "PlatformType",
+    "MeasurementType",
+    "MeasurementStyle",
+    "HomeBase",
+    "FocusArea",
+    "Season",
+    "Repository",
+    "MeasurementRegion",
+    "GeographicalRegion",
+    "GeophysicalConcept",
+    "PartnerOrg",
+    "Alias",
     "GcmdProject",
     "GcmdInstrument",
     "GcmdPlatform",
@@ -86,9 +75,9 @@ auto_url_keys = [
     "Deployment",
     "IOP",
     "SignificantEvent",
-    "CollectionPeriod", 
-    "Website", #NO INHERITANCE, title, url, website_type
-    "WebsiteType", # none
+    "CollectionPeriod",
+    "Website",
+    "WebsiteType",
     # "CampaignWebsite",
 ]
 
@@ -96,7 +85,7 @@ draft_list_urls = [
     path(
         f"{MODEL_CONFIG_MAP[model]['plural_snake_case']}/draft",
         generate_base_list_view(model),
-        name=f"{MODEL_CONFIG_MAP[model]['singular_snake_case']}-list-draft"
+        name=f"{MODEL_CONFIG_MAP[model]['singular_snake_case']}-list-draft",
     )
     for model in auto_url_keys
 ]
