5247fff03b6216a3a6ce3ed45b13f13236d895b9
pkoiralap <pkoiralap@gmail.com>
2021-10-26 19:14:28 -0500

    separate out the form data creation portion for reuse

--- admin_ui/mixins.py
+++ admin_ui/mixins.py
@@ -65,7 +65,13 @@ class ChangeModelFormMixin(ModelFormMixin):
     @property
     def destination_model_form(self):
         """ Helper to return a form for the destination of the Draft object """
-        model_type = self.get_model_form_content_type().model_class()
+        model_type = None
+        # published items use model
+        # check if it already has a model defined.
+        if self.model:
+            model_type = self.model
+        else:
+            model_type = self.get_model_form_content_type().model_class()
         modelform = modelform_factory(
             model_type,
             exclude=[],
@@ -150,6 +156,31 @@ class ChangeModelFormMixin(ModelFormMixin):
         else:
             return []
 
+    def get_update_value(self, model_form):
+        # Populate Change's form with values from destination model's form.
+        # We're not saving the cleaned_data because we want the raw text, not
+        # the processed values (e.g. we don't want Polygon objects for bounding
+        # boxes, rather we want the raw polygon text). This may or may not be
+        # the best way to achieve this.
+        update = {
+            name: field.widget.value_from_datadict(
+                model_form.data, model_form.files, model_form.add_prefix(name)
+            )
+            for name, field in model_form.fields.items()
+        }
+
+        # Save any uploaded files to disk, then overwrite their values with their name
+        for name, field in model_form.fields.items():
+            if not isinstance(field, FileField):
+                continue
+            model_field = getattr(model_form.instance, name)
+            if not model_field._file:
+                continue
+            model_field.save(model_field.url, model_form.cleaned_data[name])
+            update[name] = model_field.name
+        
+        return update
+
     def post(self, request, *args, **kwargs):
         """
         Handle POST requests: instantiate a form instance with the passed
@@ -167,28 +198,7 @@ class ChangeModelFormMixin(ModelFormMixin):
         if not form.is_valid() or (validate_model_form and not model_form.is_valid()):
             return self.form_invalid(form=form, model_form=model_form)
 
-        # Populate Change's form with values from destination model's form.
-        # We're not saving the cleaned_data because we want the raw text, not
-        # the processed values (e.g. we don't want Polygon objects for bounding
-        # boxes, rather we want the raw polygon text). This may or may not be
-        # the best way to achieve this.
-        form.instance.update = {
-            name: field.widget.value_from_datadict(
-                model_form.data, model_form.files, model_form.add_prefix(name)
-            )
-            for name, field in model_form.fields.items()
-        }
-
-        # Save any uploaded files to disk, then overwrite their values with their name
-        for name, field in model_form.fields.items():
-            if not isinstance(field, FileField):
-                continue
-            model_field = getattr(model_form.instance, name)
-            if not model_field._file:
-                continue
-            model_field.save(model_field.url, model_form.cleaned_data[name])
-            form.instance.update[name] = model_field.name
-
+        form.instance.update = self.get_update_value(model_form)
         return self.form_valid(form, model_form)
 
     def form_valid(self, form, model_form):
