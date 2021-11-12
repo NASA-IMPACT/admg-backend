4843b1f0d05bc00eeb27c5d1c7b3c19fbd3c3504
Anthony Lukach <anthonylukach@gmail.com>
2021-11-12 13:43:31 -0700

    Update code to expect parsing GCMD CSV rather than JSON

--- kms/api.py
+++ kms/api.py
@@ -1,32 +1,52 @@
-from typing import Any, Dict, Generator
+import logging
+from typing import Any, Dict, Generator, List
 import requests
 
 from . import models
 
+logger = logging.getLogger(__name__)
+
 base_url = "https://gcmdservices.gsfc.nasa.gov/kms"
 
 
-def kms_lookup(url: str) -> Dict[str, Any]:
-    r = requests.get(
-        url,
-        params={"format": "json"},
-    )
+def kms_lookup(endpoint: str, page_num=1) -> Dict[str, Any]:
+    url = f"{base_url}/{endpoint if not endpoint.startswith('/') else endpoint[1:]}"
+    logger.debug(f"Fetching {url}, page {page_num}")
+    r = requests.get(url, params={"format": "json", "page_num": page_num})
     try:
         r.raise_for_status()
     except requests.HTTPError:
-        print(r.text)
+        logger.error(f'Response from KMS: "{r.text}"')
         raise
     return r.json()
 
 
-def list_concepts(scheme: str, pattern="*") -> Generator[models.Concept, None, None]:
-    url = f"{base_url}/concepts/concept_scheme/{scheme}/pattern/{pattern}"
-    for concept in kms_lookup(url)["concepts"]:
-        yield models.Concept(
-            scheme=models.ConceptScheme(**concept.pop("scheme")),
-            definitions=[
-                models.ConceptDefinition(**definition)
-                for definition in concept.pop("definitions")
-            ],
-            **concept,
-        )
+# https://gcmdservices.gsfc.nasa.gov/kms/
+endpoints = {
+    "get_status": "/status",
+    "get_concept_fullpaths": "/concept_fullpaths/concept_uuid/${conceptId}",
+    "get_concept": "/concept/${conceptId}",
+    "get_concept_schemes": "/concept_schemes",
+    "get_concepts_by_scheme": "/concepts/concept_scheme/${conceptScheme}",
+    "get_concepts_by_scheme_pattern": "/concepts/concept_scheme/${conceptScheme}/pattern/${pattern}",
+    "get_concepts_all": "/concepts",
+    "get_concepts_root": "/concepts/root",
+    "get_concepts_by_pattern": "/concepts/pattern/${pattern}",
+    "get_concept_by_short_name": "/concept/short_name/${short_name}",
+    "get_concept_by_alt_label": "/concept/alt_label/${alt_label}",
+    "get_concept_versions": "/concept_versions/version_type/${versionType}",
+}
+
+
+def list_concepts(scheme: str) -> List[Dict[str, any]]:
+    # TODO: This should probably load data from the CSV as the CSV contains more information
+    # https://gcmd.earthdata.nasa.gov/static/kms/
+    url = f"https://gcmd.earthdata.nasa.gov/kms/concepts/concept_scheme/{scheme}"
+    r = requests.get(url, params={"format": "csv"})
+    # 2. Skip first line of CSV
+    # 3. Read with CSV DictReader
+    # 4. Return dictionary objects
+    return []
+
+def lookup_concept(uuid: str):
+    return kms_lookup(f"/concept/{uuid}")
--- kms/tasks.py
+++ kms/tasks.py
@@ -1,14 +1,42 @@
+import logging
+
 from celery import shared_task
+
 from . import api
+from api_app.models import Change
+from data_models import models
+
+logger = logging.getLogger(__name__)
+
+concept_to_model_map = {
+    "instruments": models.GcmdInstrument,
+    "projects": models.GcmdProject,
+    "platforms": models.GcmdPlatform,
+    "sciencekeywords": models.GcmdPhenomena,
+}
 
 
 @shared_task
 def sync_gcmd(concept_scheme: str):
-    for concept in api.list_concepts(concept_scheme):
-        print(concept)
+    concepts = api.list_concepts(concept_scheme)
+    
+    # TODO: sync concepts with the DB.
+
+    # If item not in local db but in API, create "ADD" change record
+    # If item in local db but not in API, create "DELETE" change record
+    # If item in local db and in API and they are not the same, create "UPDATE" change record
+    
+    return f"Handled {len(concepts)} {concept_scheme}"
 
 
 @shared_task
 def trigger_gcmd_syncs():
-    gcmd_concepts = ["Projects", "Instruments", "Platforms", "Phenomena"]
-    return [sync_gcmd.delay(concept) for concept in gcmd_concepts]
+    """
+    Triggers tasks to sync each GCMD concept of interest. Tasks are scheduled
+    in a staggered fashion to reduce load on systems.
+    """
+    offset = 60 * 5  # Use an offset to avoid overwhelming KMS API
+    for i, concept in enumerate(concept_to_model_map):
+        delay = i * offset
+        logger.info(f"Scheduling task to sync {concept} in {delay} seconds")
+        sync_gcmd.apply_async(args=(concept,), countdown=delay, retry=False)
