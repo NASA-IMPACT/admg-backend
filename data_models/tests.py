1e2361468d3fb7c0852135a10d266fb88afd99ce
Anthony Lukach <anthonylukach@gmail.com>
2022-01-28 15:37:52 -0800

    Add failing test

--- data_models/tests.py
+++ data_models/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import serializers
+
+
+class TestImageSerializers(TestCase):
+    def test_draft(self):
+        serializer = serializers.ImageSerializer(
+            data={
+                "description": "Let's see if this will publish",
+                "image": "1438940e-94b7-440d-9658-ba9e9a85c0ef.jpeg",
+                "owner": "",
+                "source_url": "",
+                "title": "Test Image",
+            }
+        )
+        serializer.is_valid(raise_exception=True)
