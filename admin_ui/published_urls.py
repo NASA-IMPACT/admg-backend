ff69ec26158d8eb89dae4092ba6d15fdb16f1919
Anthony Lukach <anthonylukach@gmail.com>
2022-04-05 16:14:07 -0700

    Rm unused view

--- admin_ui/published_urls.py
+++ admin_ui/published_urls.py
@@ -1,37 +1,24 @@
 from django.urls import path
 
 from . import published_views
-from .config import MODEL_CONFIG_MAP
 
 published_urls = [
-    *[
-        route
-        for model, config in MODEL_CONFIG_MAP.items()
-        for route in [
-            # publish urls
-            path(
-                f"{config['plural_snake_case']}/published",
-                published_views.GenericListView(model).as_view(),
-                name=f"{config['singular_snake_case']}-list-published",
-            ),
-            # detail urls
-            path(
-                f"{config['plural_snake_case']}/published/<uuid:pk>",
-                published_views.GenericDetailView(model).as_view(),
-                name=f"{config['singular_snake_case']}-detail-published",
-            ),
-            # edit urls
-            path(
-                f"{config['plural_snake_case']}/published/<uuid:pk>/edit",
-                published_views.GenericEditView(model).as_view(),
-                name=f"{config['singular_snake_case']}-edit-published",
-            ),
-            # delete urls
-            path(
-                f"{MODEL_CONFIG_MAP[model]['plural_snake_case']}/published/<uuid:pk>/delete",
-                published_views.GenericDeleteView(model).as_view(),
-                name=f"{MODEL_CONFIG_MAP[model]['singular_snake_case']}-delete-published",
-            ),
-        ]
-    ]
+    path(
+        f"published/<str:model>", published_views.GenericListView.as_view(), name="published-list"
+    ),
+    path(
+        f"published/<str:model>/<uuid:pk>",
+        published_views.GenericDetailView.as_view(),
+        name="published-detail",
+    ),
+    path(
+        f"published/<str:model>/<uuid:pk>/edit",
+        published_views.GenericEditView.as_view(),
+        name="published-edit",
+    ),
+    path(
+        f"published/<str:model>/<uuid:pk>/delete",
+        published_views.GenericDeleteView.as_view(),
+        name="published-delete",
+    ),
 ]
