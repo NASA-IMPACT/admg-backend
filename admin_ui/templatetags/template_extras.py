828b60d6a89a9168d2f8ff5c35fed8bafee65a40
Anthony Lukach <anthonylukach@gmail.com>
2022-04-05 18:31:21 -0700

    Rm tmp work

--- admin_ui/templatetags/template_extras.py
+++ admin_ui/templatetags/template_extras.py
@@ -1,5 +1,4 @@
 from django import template
-from django.urls import reverse
 
 from admin_ui.config import MODEL_CONFIG_MAP
 
@@ -16,9 +15,3 @@ def get_display_name(value):
     if "display_name" in MODEL_CONFIG_MAP.get(value, {}):
         return MODEL_CONFIG_MAP[value]["display_name"]
     return value
-
-
-# @register.filter
-# def get_published_url(value):
-#     reverse(f'published-{}-detail', args=(value.model_instance_uuid,))
-#     MODEL_CONFIG_MAP[value]["display_name"]
