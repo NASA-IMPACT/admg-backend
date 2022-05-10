c60d1a77075e3898ca4404ef65c81413a0a27b27
Carson Davis <carsonreiddavis@gmail.com>
2022-05-10 09:29:03 -0500

    lint migration

--- data_models/migrations/0045_auto_20220509_1448.py
+++ data_models/migrations/0045_auto_20220509_1448.py
@@ -6,20 +6,21 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('data_models', '0044_auto_20220203_1500'),
-    ]
+    dependencies = [('data_models', '0044_auto_20220203_1500')]
 
     operations = [
         migrations.AddField(
-            model_name='doi',
-            name='cmr_abstract',
-            field=models.TextField(blank=True, default=''),
+            model_name='doi', name='cmr_abstract', field=models.TextField(blank=True, default='')
         ),
         migrations.AddField(
             model_name='doi',
             name='cmr_data_formats',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(blank=True, default='', max_length=512), blank=True, default=list, size=None),
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(blank=True, default='', max_length=512),
+                blank=True,
+                default=list,
+                size=None,
+            ),
         ),
         migrations.AddField(
             model_name='doi',
