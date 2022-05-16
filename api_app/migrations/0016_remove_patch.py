cdb2b588794c07d2f380bb8d5e2ab963237b007e
Carson Davis <carsonreiddavis@gmail.com>
2022-05-16 10:42:56 -0500

    migrate and remove old instances of patch

---
+++ api_app/migrations/0016_remove_patch.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+from api_app.models import Change
+
+
+def transition_patch_to_update():
+    Change.objects.filter(action='Patch').update(action=Change.Actions.UPDATE)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [('api_app', '0015_change_field_status_tracking')]
+
+    operations = [
+        migrations.RunPython(transition_patch_to_update, reverse_code=migrations.RunPython.noop),
+        migrations.AlterField(
+            model_name='change',
+            name='action',
+            field=models.CharField(
+                choices=[('Create', 'Create'), ('Update', 'Update'), ('Delete', 'Delete')],
+                default='Update',
+                max_length=10,
+            ),
+        ),
+    ]
