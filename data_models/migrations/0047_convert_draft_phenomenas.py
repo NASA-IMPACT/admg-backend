65e39d341e34903bfe9d0ad0b77f6e5cc21178a9
Carson Davis <carsonreiddavis@gmail.com>
2022-06-06 14:36:12 -0500

    add api_app dependency

--- data_models/migrations/0047_convert_draft_phenomenas.py
+++ data_models/migrations/0047_convert_draft_phenomenas.py
@@ -2,12 +2,10 @@
 
 from django.db import migrations
 from api_app.models import Change
-from django.contrib.contenttypes.models import ContentType
 
 
 def convert_phenomenas_to_phenomena(apps, schema_editor):
-    instrument_content_type = ContentType.objects.get(model='instrument')
-    instrument_drafts = Change.objects.filter(content_type=instrument_content_type)
+    instrument_drafts = Change.objects.filter(content_type__model="instrument")
     for instrument_draft in instrument_drafts:
         try:
             instrument_draft.update['gcmd_phenomena'] = instrument_draft.update.pop(
@@ -20,7 +18,10 @@ def convert_phenomenas_to_phenomena(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    dependencies = [("data_models", "0046_auto_20220526_1047")]
+    dependencies = [
+        ("data_models", "0046_auto_20220526_1047"),
+        ("api_app", "0015_change_field_status_tracking"),
+    ]
 
     operations = [
         migrations.RunPython(
