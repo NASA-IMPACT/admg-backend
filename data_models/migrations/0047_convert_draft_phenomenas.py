fa354ecb77f8bb8112086a97d3e9496e082ccefe
Carson Davis <carsonreiddavis@gmail.com>
2022-06-06 13:33:10 -0500

    use ContentType to access drafts and add KeyError check

--- data_models/migrations/0047_convert_draft_phenomenas.py
+++ data_models/migrations/0047_convert_draft_phenomenas.py
@@ -2,13 +2,20 @@
 
 from django.db import migrations
 from api_app.models import Change
+from django.contrib.contenttypes.models import ContentType
 
 
 def convert_phenomenas_to_phenomena(apps, schema_editor):
-    instrument_drafts = Change.objects.filter(model_name='Instrument')
+    instrument_content_type = ContentType.objects.get(model='instrument')
+    instrument_drafts = Change.objects.filter(content_type=instrument_content_type)
     for instrument_draft in instrument_drafts:
-        instrument_draft.update['gcmd_phenomena'] = instrument_draft.update.pop('gcmd_phenomenas')
-        instrument_draft.save()
+        try:
+            instrument_draft.update['gcmd_phenomena'] = instrument_draft.update.pop(
+                'gcmd_phenomenas'
+            )
+            instrument_draft.save()
+        except KeyError:
+            pass
 
 
 class Migration(migrations.Migration):
