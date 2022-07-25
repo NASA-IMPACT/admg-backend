6814ba23534e91f996546abdf0430fd672c70bbf
Edward Keeble <edward@developmentseed.org>
2022-07-25 13:42:30 -0300

    Updated coverage packages to latest versions. Updated coverage config to measure the correct modules and enable relative reporting. Updated test config to enable coverage for template files. Updated README to include instructions for running coverage.

--- config/settings/test.py
+++ config/settings/test.py
@@ -44,6 +44,8 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
     )
 ]
 
+TEMPLATES[0]["OPTIONS"]["debug"] = True
+
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
