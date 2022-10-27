3ac34712cf2bd7c3bfa2eceae4d604d4a6c19c24
Carson Davis <carsonreiddavis@gmail.com>
2022-10-27 16:29:10 -0500

    remove duplicate CampaignFactory

--- admin_ui/tests/factories.py
+++ admin_ui/tests/factories.py
@@ -1,24 +1,9 @@
-from datetime import date
-
 import factory
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 
 from admg_webapp.users.models import User
 from api_app.models import Change
-from data_models.models import Campaign
-
-
-class CampaignFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Campaign
-
-    start_date = date(year=2022, month=1, day=1)
-    region_description = "test"
-    ongoing = True
-    nasa_led = True
-    lead_investigator = "test"
-    focus_phenomena = "test"
 
 
 class ChangeFactory(factory.django.DjangoModelFactory):
