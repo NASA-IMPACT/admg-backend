6c77d50a1746b7b39e57527c18fe1c24126f3e15
Edward Keeble <edward@developmentseed.org>
2022-11-09 11:06:16 -0400

    Updated deployment config and requirements to enable sentry.

--- config/settings/production.py
+++ config/settings/production.py
@@ -169,5 +169,5 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+    traces_sample_rate=int(env("SENTRY_TRACES_SAMPLE_RATE", default=1.0)),
 )
