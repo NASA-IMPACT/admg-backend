1f75d11368008b15528def3930bca6e30180da77
Carson Davis <CarsonReidDavis@gmail.com>
2022-12-20 15:45:43 -0600

    Update api_app/models.py

--- api_app/models.py
+++ api_app/models.py
@@ -793,7 +793,7 @@ class Change(models.Model):
 
         # check if unclaiming user is the same as the claiming user or if unclaiming user is admin
         latest_log = self.get_latest_log()
-        if user.get_role_display() != User.Roles.ADMIN.label:
+        if user.role != User.Roles.ADMIN:
             if latest_log.user != user:
                 return generate_failure_response(
                     "To unclaim an item the user must be the same as the claiming user,"
