9ff451e0c5c16ae585912e5b866fb552cf7ab07c
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2022-12-22 15:14:53 -0800

    Fill out campaign creation form

--- admin_ui/tests/e2e/test_workflow.py
+++ admin_ui/tests/e2e/test_workflow.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from ..factories import UserFactory
 
-from playwright.sync_api import Browser, StorageState, sync_playwright
+from playwright.sync_api import Browser, StorageState, sync_playwright, expect
 
 
 class BrowserContextManager:
@@ -20,7 +20,7 @@ class BrowserContextManager:
 
     def __enter__(self):
         self.context = self.browser.new_context(
-            storage_state=self.storage_state, base_url=self.base_url
+            storage_state=self.storage_state, base_url=self.base_url, record_video_dir="videos/"
         )
         print("Context cookies")
         print(self.context.cookies())
@@ -80,13 +80,14 @@ class CuratorWorkflowTests(StaticLiveServerTestCase):
             * Campaign Short Name
             * Start date
             * Region description
-            * Season(s) of Study
             * Focus phenomena
-            * NASA Earth Science Focus Area(s)
-            * Geophysical concepts
             * Principal Investigator
             * Is this field investigation currently ongoing?
             * Is this a NASA-led field investigation?
+
+            * Season(s) of Study
+            * NASA Earth Science Focus Area(s)
+            * Geophysical concepts
             * Platform types
             * Data Repository(ies)
         4. Click Save button
@@ -100,6 +101,27 @@ class CuratorWorkflowTests(StaticLiveServerTestCase):
             page.wait_for_url(
                 f"{self.live_server_url}{reverse('change-list', kwargs={'model':'campaign'})}"
             )
+            # Navigate to new campaign creation
+            page.locator("a", has_text="Add New Campaign +").click()
+            expect(page.locator("h1", has_text="Add new Campaign draft")).to_be_visible()
+
+            # Fill in Campaign input fields
+            page.fill("[name=model_form-short_name]", "test_campaign")
+            page.fill("[name=model_form-start_date]", "2022-12-22")
+            page.fill("[name=model_form-region_description]", "region description")
+            page.fill("[name=model_form-lead_investigator]", "Ali Gator")
+            page.fill("[name=model_form-focus_phenomena]", "test focus phenomenon")
+
+            # Select boolean dropdowns
+            page.select_option("select#id_model_form-ongoing", label="Yes")
+            page.select_option("select#id_model_form-nasa_led", label="Yes")
+
+            # select dropdowns based on db options
+            # page.select_option("select#id_model_form-seasons", label="?")
+            # page.select_option("select#id_model_form-focus_areas", label="?")
+            # page.select_option("select#id_model_form-geophysical_concepts", label="?")
+            # page.select_option("select#id_model_form-platform_types", label="?")
+            # page.select_option("select#id_model_form-repositories", label="?")
 
     def test_login(self):
         with BrowserContextManager(
