500d13208e32dad12de47a066862d73a4ffa9f0c
Praveenvijayakumar  Phatate <pvp0001@CS.UAH.EDU>
2023-03-07 13:27:28 -0600

    Fixed Flake error

--- doi_update.py
+++ doi_update.py
@@ -1,6 +1,10 @@
 import requests
 import json
 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+from api_app.models import Change
+from data_models.models import DOI
 
 def process_cmr(concept_id):
     """Queries CMR for a specific concept_id and returns
