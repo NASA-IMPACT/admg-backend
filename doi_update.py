9c313d9af35af955630b7d730655512be94488ad
Praveenvijayakumar  Phatate <pvp0001@CS.UAH.EDU>
2023-03-07 11:53:09 -0600

    Updated the function and works

--- doi_update.py
+++ doi_update.py
@@ -1,41 +1,68 @@
 import requests
 import json
-from cmr.process_metadata import process_metadata_list
-base_url = "https://cmr.earthdata.nasa.gov/search/collections.umm_json?echo_collection_id=" 
-all_dois = DOI.objects.all()
-notes = "abstract and thing updated by t…."
-user = User.objects.get(username = "nimda")
-for doi_item in all_dois:
-    concept_id = doi_item.concept_id
-    #print(concept_id)
+
+def process_cmr(concept_id):
+    """Queries CMR for a specific concept_id and returns
+    the dictionary conatining cmr abstract and data_formats.
+
+    Args:
+        concept_id (str): concept_id of DOI
+
+    Returns:
+        dict: dict containing cmr_abstract and crm_data_formats
+    """
+    base_url = "https://cmr.earthdata.nasa.gov/search/collections.umm_json?echo_collection_id=" 
     cmr_url = base_url + concept_id
-    response = requests.get(cmr_url)
-    concept_ids_nested = [collection["meta"]["concept-id"] for collection in response.json()["items"]]
-    concept_id_data = response.json()["items"]
-    processed_metadata_list = process_metadata_list(concept_id_data)
-    #print(processed_metadata_list)
-    # Check if processed meta data list is empty
-    if processed_metadata_list:
-        #print("List is not empty")
-        json_str = {'cmr_abstract' : processed_metadata_list[0]['cmr_abstract'], 'cmr_data_formats' : processed_metadata_list[0]['cmr_data_formats']}
-        json_obj = json.dumps(json_str)
-        # This is the issue need to update model_instance_uuid
-        draft = Change.objects.of_type(DOI).get(uuid = doi_item.uuid)
-        model_instance_uuid = draft.model_instance_uuid
-        # model_instance_uuid = Change.objects.of_type(DOI).get(uuid = doi_item.uuid)
-        # Check if json_str and draft abstract and data formats are same
+    query_response = requests.get(cmr_url)
+    query_value = query_response.json()["items"]
+    # Check if the Earth data returned anything
+    if query_value: 
+        return {
+            "cmr_abstract": query_value[0]["umm"].get("Abstract", ''),
+            "cmr_data_formats": [
+                info.get('Format', '')
+                for info in query_value[0]["umm"]
+                .get('ArchiveAndDistributionInformation', {})
+                .get('FileDistributionInformation', [{}])
+            ],
+        }
+    else:
+        return {}
+    
+def make_update_publish_doi_draft(doi_uuid, update_value):
+    """ Creates a draft for a doi, using doi_uuid and update_vlaue which is dictonary and
+    published the draft
 
-        doi_obj = Change(
-                content_type=doi_item.uuid, 
-                model_instance_uuid = model_instance_uuid,
-                update=json.loads(json_obj),
-                status=Change.Statuses.CREATED,
-                action=Change.Actions.UPDATE,
-                 )
-        doi_obj.save()
-        doi_obj.publish(user, notes=notes)
+    Args:
+        doi_uuid (uuid): uuid of DOI
+        update_value(json): json dict containing astract and data_formats
 
+    Returns:
+        None:
+    """
+    notes = "abstract and thing updated by t…."
+    user = User.objects.get(username = "nimda")
+    draft = Change.objects.create(
+        content_type = ContentType.objects.get_for_model(DOI),
+        status = Change.Statuses.CREATED,
+        action = Change.Actions.UPDATE,
+        model_instance_uuid = doi_uuid,
+        update = update_value
+        )
+    draft.save()
+    draft.publish(user, notes = notes)
 
+def update_dois():
+    """ Queries the database for a uuid within a DOI table name, and 
+        queries CRM to get the dict and updates the main db
+    """
+    dois = DOI.objects.all()
+    for doi in dois:
+        processed_metadata_list = process_cmr(doi.concept_id)
+        # Check if processed meta data list is not empty
+        if processed_metadata_list:
+            update_value = json.loads(json.dumps(processed_metadata_list))
+            make_update_publish_doi_draft(doi.uuid, update_value)
 
-
-
+# Run DOI Update
+update_dois()
