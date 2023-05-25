e4d5c80bf5cce68c1f9ed0ce7c0b24e7ad059259
Lint Action <lint-action@samuelmeuli.com>
2023-05-25 19:53:48 +0000

    Fix code style issues with Black

--- cmr/tests/urlfile.py
+++ cmr/tests/urlfile.py
@@ -1,11 +1,13 @@
 from data_models.models import Campaign
 from cmr.doi_matching import DoiMatcher
 
-ascends_uuid = str(Campaign.objects.get(short_name = 'ASCENDS Airborne').uuid)
-ascends_doi_drafts = Change.objects.of_type(DOI).filter(update__contains={'campaigns': [ascends_uuid]})
+ascends_uuid = str(Campaign.objects.get(short_name='ASCENDS Airborne').uuid)
+ascends_doi_drafts = Change.objects.of_type(DOI).filter(
+    update__contains={'campaigns': [ascends_uuid]}
+)
 for draft in ascends_doi_drafts:
-	print(draft.uuid, draft.status, draft.action, draft.update['cmr_short_name'], draft.updated_at)
-	
+    print(draft.uuid, draft.status, draft.action, draft.update['cmr_short_name'], draft.updated_at)
+
 """
 2521a1a5-425a-4c74-ae60-1e6a8fba2ca2 6 Create ABoVE_ASCENDS_Backscatter_2051 2022-12-20 14:56:38.091000+00:00
 50152972-5c67-4aa2-babf-610a1885db81 6 Create ABoVE_ASCENDS_XCO2_2050 2022-12-20 14:56:27.026000+00:00
@@ -16,10 +18,12 @@ matcher = DoiMatcher()
 matcher.generate_recommendations('campaign', ascends_uuid)
 
 # Print all the drafts associated with ASCENDS
-ascends_doi_drafts = Change.objects.of_type(DOI).filter(update__contains={'campaigns': [ascends_uuid]})
+ascends_doi_drafts = Change.objects.of_type(DOI).filter(
+    update__contains={'campaigns': [ascends_uuid]}
+)
 for draft in ascends_doi_drafts:
-	print(draft.uuid, draft.status, draft.action, draft.update['cmr_short_name'], draft.updated_at)
-	
+    print(draft.uuid, draft.status, draft.action, draft.update['cmr_short_name'], draft.updated_at)
+
 """
 db8cd0d8-e861-49fa-9170-4c5c43b15f63 6 Update ABoVE_ASCENDS_XCO2_2050 None
 912a4e79-ad20-4fe1-827b-9fac14bedc90 6 Update ABoVE_ASCENDS_Backscatter_2051 None
@@ -35,9 +39,11 @@ matcher = DoiMatcher()
 matcher.generate_recommendations('campaign', ascends_uuid)
 
 # Print all the drafts associated with ASCENDS
-ascends_doi_drafts = Change.objects.of_type(DOI).filter(update__contains={'campaigns': [ascends_uuid]})
+ascends_doi_drafts = Change.objects.of_type(DOI).filter(
+    update__contains={'campaigns': [ascends_uuid]}
+)
 for draft in ascends_doi_drafts:
-	print(draft.uuid, draft.status, draft.action, draft.update['cmr_short_name'], draft.updated_at)
+    print(draft.uuid, draft.status, draft.action, draft.update['cmr_short_name'], draft.updated_at)
 
 # No Changes in the drafts
 
