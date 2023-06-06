ed57889e7f93bbd8915c1b672eed2fa3144e4391
Carson Davis <carsonreiddavis@gmail.com>
2023-06-06 14:55:15 -0500

    manually handle database object refresh

--- cmr/doi_matching.py
+++ cmr/doi_matching.py
@@ -367,6 +367,7 @@ class DoiMatcher:
             status=Change.Statuses.CREATED,
             action=Change.Actions.CREATE,
         )
+        doi_obj = Change.objects.get(uuid=doi_obj.uuid)
         return doi_obj
 
     @staticmethod
@@ -378,6 +379,7 @@ class DoiMatcher:
             status=Change.Statuses.CREATED,
             action=Change.Actions.UPDATE,
         )
+        doi_obj = Change.objects.get(uuid=doi_obj.uuid)
         return doi_obj
 
     @staticmethod
