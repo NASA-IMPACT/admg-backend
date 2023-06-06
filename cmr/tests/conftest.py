141a5e8c030f062fc6124513a7410b7b5cec4697
Carson Davis <CarsonReidDavis@gmail.com>
2023-06-06 14:06:33 -0500

    Update cmr/tests/conftest.py

--- cmr/tests/conftest.py
+++ cmr/tests/conftest.py
@@ -7,7 +7,4 @@ from django.contrib.contenttypes.models import ContentType
 def load_test_data(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         ContentType.objects.all().delete()
-        # does call_command use the current context? test database or something else
-        # maybe we should be loading this fixture in a different way
-        # try to load dev_dump into a empty docker
         call_command('loaddata', 'cmr/fixtures/stage_backup_2023.05.23.json')
