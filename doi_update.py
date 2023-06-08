3dd0a6437d86e02d5c4f3d13eca74cffc2188985
Praveenvijayakumar  Phatate <pvp0001@CS.UAH.EDU>
2023-06-08 13:44:29 -0500

    Updated the doi_update to handle missing concept_id

--- doi_update.py
+++ doi_update.py
@@ -1,30 +1,46 @@
 import requests
-import json
 
-# from django.contrib.auth.models import User
-from admg_webapp.users.models import User
-from data_models.models import DOI
 from cmr.doi_matching import DoiMatcher
 from cmr.process_metadata import process_data_product
-
+from data_models.models import DOI
 
 def query_cmr_for_concept_id(concept_id):
-    """Queries CMR for a specific concept_id and returns
-    cmr response.
+    """Returns the cmr response given a concept_id
 
     Args:
-        concept_id (str): concept_id of DOI
+        concept_id (string): concept_id
 
     Returns:
-        dict: json dict containing cmr_abstract and crm_data_formats
+        cmr_data: Json containing the cmr response
+        hits: Number of hits in the cmr page
     """
     base_url = "https://cmr.earthdata.nasa.gov/search/collections.umm_json?echo_collection_id="
     cmr_url = base_url + concept_id
     query_response = requests.get(cmr_url)
     cmr_data = query_response.json()["items"]
+    hits = query_response.json()["hits"]
     query_response.close()
-    return cmr_data
 
+    return cmr_data, hits
+
+def query_cmr_for_doi(doi):
+    """Returns the cmr response given a doi
+
+    Args:
+        doi (string): doi
+
+    Returns:
+        cmr_data: Json containing the cmr response
+        hits: Number of hits in the cmr page
+    """
+    base_url = "https://cmr.earthdata.nasa.gov/search/collections.umm_json?doi="
+    cmr_url = base_url + doi
+    query_response = requests.get(cmr_url)
+    cmr_data = query_response.json()["items"]
+    hits = query_response.json()["hits"]
+    query_response.close()
+
+    return cmr_data, hits
 
 def convert_cmr_data_to_doi_recommendation(cmr_data):
     return process_data_product(cmr_data)
@@ -32,14 +48,20 @@ def convert_cmr_data_to_doi_recommendation(cmr_data):
 
 def update_entire_database():
     matcher = DoiMatcher()
-    list_of_dois = DOI.objects.all()
-    for doi in list_of_dois:
-        cmr_data = query_cmr_for_concept_id(doi.concept_id)
-        doi_recommendation = convert_cmr_data_to_doi_recommendation(cmr_data[0])
-        try:
-            matcher.add_to_db(doi_recommendation)
-        except:
-            print(doi.concept_id)
-
-
-update_entire_database()
+    missing_dois = []
+    for doi in DOI.objects.all():
+        cmr_data, hits = query_cmr_for_concept_id(doi.concept_id)
+        if not cmr_data:
+            cmr_data, hits = query_cmr_for_doi(doi.doi)
+        # Check the number of hits
+        if hits == 1:
+            doi_recommendation = convert_cmr_data_to_doi_recommendation(cmr_data[0])
+            matcher.add_to_db(doi_recommendation) 
+        
+        else:
+            missing_dois.append(doi)
+    # If there are any DOI's which are not updated
+    if missing_dois:
+        with open("dois_not_updated.txt", "w") as file:
+            for doi in missing_dois:
+                file.write(f"{doi}\n")
