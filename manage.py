1261427ff1d69412eacc98ff0c0b60dec6bf417b
Anthony Lukach <anthonylukach@gmail.com>
2023-06-29 13:06:37 -0700

    Add tooling to support debugging docker instance in VSCode

--- manage.py
+++ manage.py
@@ -2,8 +2,18 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 3000))
+            print('Attached Debugger at 0.0.0.0:3000')
 
     try:
         from django.core.management import execute_from_command_line
