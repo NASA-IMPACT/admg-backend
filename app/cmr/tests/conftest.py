6693ec7359d2aea44cb91dc25928a812f5c1a32b
Anthony Lukach <anthonylukach@gmail.com>
2023-09-18 11:42:56 -0700

    Simplify

--- app/cmr/tests/conftest.py
+++ app/cmr/tests/conftest.py
@@ -1,10 +1,8 @@
 import pytest
 from django.core.management import call_command
-from django.contrib.contenttypes.models import ContentType
 
 
 @pytest.fixture(scope='session')
 def load_test_data(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        ContentType.objects.all().delete()
         call_command('loaddata', 'cmr/fixtures/stage_backup_2023.05.23.slim.json')
